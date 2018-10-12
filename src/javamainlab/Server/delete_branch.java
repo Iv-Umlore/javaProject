3049578d92c4af3f4f926c90f2df0f8dccfc0675
Umlore <iv.kiselev1998@yandex.ru>
2018-10-12 14:11:51 +0400

    First point

---
+++ src/javamainlab/Server/delete_branch.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javamainlab.Server;
+
+/**
+ *
+ * @author Umlore
+ */
+public class delete_branch {
+    
+}
