964f70ff88faf931240936861e3b997febd1753a
Iv-Umlore <iv.kiselev1998@yandex.ru>
2018-10-20 16:31:19 +0300

    New Tasks

--- src/javamainlab/Server/Server.java
+++ src/javamainlab/Server/Server.java
@@ -14,7 +14,6 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Hashtable;
-import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
